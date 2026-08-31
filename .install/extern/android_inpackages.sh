3f984557910ac50f35c61c0b6f9a51556b677f11
Zeronetsec <senxhunter996@gmail.com>
2026-08-31 16:47:01 +0700

    modified: .install/extern/android_inpackages.sh

--- .install/extern/android_inpackages.sh
+++ .install/extern/android_inpackages.sh
@@ -10,16 +10,18 @@ function install::extern::androidInpackages() {
         echo -e "${color_B}[*] ${color_N}Installing: ${color_GG}${line}${color_N}"
         install::zinstall "${line}"
 
-        if [[ "${line}" =~ ^(tur-repo|glibc-repo)$ ]]; then
-            command apt update -y \
-                > /dev/null 2>&1
-            export DEBIAN_FRONTEND=noninteractive
-            command apt \
-                -o Dpkg::Options::="--force-confdef" \
-                -o Dpkg::Options::="--force-confold" \
-                -o Dpkg::Options::="--force-overwrite" \
-                full-upgrade -y \
-                > /dev/null 2>&1
+        if [[ "${line}" =~ ^(glibc-repo)$ ]]; then
+            install::getinstall \
+                "
+                    command apt update -y
+                    export DEBIAN_FRONTEND=noninteractive
+                    command apt \
+                        -o Dpkg::Options::=\"--force-confdef\" \
+                        -o Dpkg::Options::=\"--force-confold\" \
+                        -o Dpkg::Options::=\"--force-overwrite\" \
+                        full-upgrade -y
+                " \
+                "Upgrading environment..."
         fi
     done
 }; readonly -f install::extern::androidInpackages
